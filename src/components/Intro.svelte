7b7763bc4438d80a7b47e5ea1c9eaf31cb5060cd
Piotr Kowalski <piecioshka@gmail.com>
2019-09-23 11:49:19 +0200

    Add date

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -7,13 +7,21 @@
     /* background: #ECEFF4; */
     padding: 20px 20px 100px 20px;
     text-align: center;
+    color: #2e3440;
 }
 .logo-confrontjs {
     height: 300px;
 }
 p {
-    font-size: 40px;
     margin: 0;
+}
+p.headline {
+    font-weight: 800;
+    font-size: 50px;
+}
+p.subline {
+    font-size: 30px;
+    font-weight: 300;
 }
 </style>
 
@@ -23,6 +31,8 @@ p {
         <use xlink:href="#icon-confrontjs"></use>
     </svg>
 
-    <p>Front-end conference 2019 in Warsaw</p>
+    <p class="headline">Front-end conference 2019</p>
+
+    <p class="subline">7 December 2019 in Warsaw, Poland</p>
 
 </div>
