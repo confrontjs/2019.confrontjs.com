cbeebb0d715b83984ef16519ef4ab3f3b07cc178
Piotr Kowalski <piecioshka@gmail.com>
2019-11-12 14:22:26 +0100

    Update speaker bio

--- src/data/speakers/tomasz-lakomy.js
+++ src/data/speakers/tomasz-lakomy.js
@@ -4,12 +4,12 @@ module.exports = {
     company: 'OLX',
     country: 'Poland',
     bio: {
-        en: ``
+        en: "Tomasz is a Senior Frontend Developer at OLX Group. Tech speaker, egghead.io instructor, jQuery evangelist. His interests include React, VR, testing, app performance and making cheesiest jokes known to mankind."
     },
     avatar_url: '/speakers/tomasz-lakomy.jpg',
     contact: {
         linkedin_url: 'https://www.linkedin.com/in/tomasz-%C5%82akomy-12b2a258/',
         twitter_url: 'https://twitter.com/tlakomy',
-        github_url: ''
+        github_url: 'https://github.com/tlakomy'
     }
 };
